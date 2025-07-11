3b3932dc6d34d334c82c35e3b7da14bf3ba837b7
casilimas <freddycasilimas@gmail.com>
2025-07-11 06:11:27 -0400

    ♻️ Eliminar scroll externo en WorkerDashboard y ajustar scroll solo en Cajanegra

--- src/pages/dashboards/WorkerDashboard.jsx
+++ src/pages/dashboards/WorkerDashboard.jsx
@@ -9,43 +9,32 @@ import CajATrabajadorAmarilla from "../../movil/barras/CajaTrabajadorAmailla";
 
 const WorkerDashboard = () => {
   return (
-    <div className="relative min-h-screen bg-white text-black md:text-white transition-all duration-300 overflow-x-hidden flex flex-col md:flex-row">
-
-      {/* 🟩 Caja verde arriba, solo visible en sm */}
-    <div className="block md:hidden w-full">
+    <div className="relative min-h-screen bg-white text-black md:text-white transition-all duration-300 overflow-hidden flex flex-col md:flex-row">
+      
+      {/* 🟩 Caja verde móvil (solo visible en sm) */}
+      <div className="block md:hidden w-full">
         <CajaVerdeTrabajador />
         <CajaVioletaTrabajador />
         <CajATrabajadorAmarilla /> 
       </div>
-      
 
       {/* 🔹 Columna izquierda con UserSummary */}
       <div className="min-h-screen md:w-[500px] bg-white p-6 overflow-x-hidden">
         <UserSummary />
         <CajaAzul />
-
       </div>
 
-
-    
-
-
-
-
-
-
-      
-
-      {/* 🔸 Columna derecha: NavbarJefe + Cajanegra */}
+      {/* 🔸 Columna derecha */}
       <div className="flex flex-col w-full">
         <div className="h-[190px] w-full bg-white p-0 overflow-x-hidden flex items-center justify-center">
           <NavbarJefe />
         </div>
 
-        <div className="w-full">
+        {/* ⬇️ Scroll solo aquí */}
+        <div className="flex-1 h-[calc(100vh-190px)] overflow-y-auto w-full">
           <Cajanegra />
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
