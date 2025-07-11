a67a9a97208896fe6f41e7d9619062865dd762b8
casilimas <freddycasilimas@gmail.com>
2025-07-11 06:00:10 -0400

    🛠️ Eliminar scroll global y mantener scroll solo en Cajanegra (BossDashboard)

--- src/pages/dashboards/BossDashboard.jsx
+++ src/pages/dashboards/BossDashboard.jsx
@@ -9,10 +9,8 @@ import CajAJefeAmarilla from "../../movil/barras/CajaJefeAmarilla";
 
 const BossDashboard = () => {
   return (
-    <div className="relative min-h-screen bg-white text-black md:text-white transition-all duration-300 overflow-x-hidden flex flex-col md:flex-row">
+    <div className="relative h-screen overflow-hidden bg-white text-black md:text-white transition-all duration-300 flex flex-col md:flex-row">
       
-
-
       {/* 🟩 Caja verde arriba, solo visible en sm */}
       <div className="block md:hidden w-full">
         <Cajaverde />
@@ -20,23 +18,20 @@ const BossDashboard = () => {
         <CajAJefeAmarilla /> 
       </div>
 
-
-
-
-
       {/* 🔹 Columna izquierda con UserSummary y CajaAzul */}
-      <div className="min-h-screen md:w-[500px] bg-white p-6 overflow-x-hidden">
+      <div className="hidden md:block min-h-screen md:w-[500px] bg-white p-6 overflow-x-hidden">
         <UserSummary />
         <CajaAzul />
       </div>
 
       {/* 🔸 Columna derecha: NavbarJefe + Cajanegra */}
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full h-full">
         <div className="h-[190px] w-full bg-white p-0 overflow-x-hidden">
           <NavbarJefe />
         </div>
 
-        <div className="w-full">
+        {/* ✅ Scroll SOLO aquí */}
+        <div className="w-full h-[calc(100vh-190px)] overflow-y-auto">
           <Cajanegra />
         </div>
       </div>
